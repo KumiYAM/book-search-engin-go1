9a2533a746172d742d4e67bb504df8bcd4679b82
kumikoyamagata <sun.kumiko@protonmail.com>
2022-01-18 15:25:54 -0600

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/bookdb", {
   useNewUrlParser: true,
   useUnifiedTopology: true
   //useCreateIndex: true,
   //useFindAndModify: false
 });
+module.exports = mongoose.connection;
